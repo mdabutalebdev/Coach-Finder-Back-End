e42c24ca3ab26a3c2d7099e7b9d51982930436b5
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-12-21 19:38:43 +0600

    x

--- src/Components/addInfo.jsx
+++ src/Components/addInfo.jsx
@@ -86,8 +86,6 @@ const AddInfo = () => {
   const infoSubmit = (e) => {
     e.preventDefault();
 
-    console.log("groupInfo", groupInfo);
-
     let token = localStorage.getItem("adminAuthToken");
     async function infoFunc() {
       try {
